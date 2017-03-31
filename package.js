e0558208c78cba69ff6ceb70b2a545cf0529988d
Aaron Louie <ajlouie@gmail.com>
2017-03-31 10:58:52 -0400

    Updates version numbers

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Fork of marcuzy/meteor-synced-cron. Define and run scheduled jobs across multiple servers.',
-  version: '0.1',
+  version: '1.0.0',
   name: 'ajlouie:meteor-synced-cron',
   git: 'https://github.com/ajlouie/meteor-synced-cron.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Npm.depends({later: '1.2.0', timezone:'1.0.6'});
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.4.0');
+  api.versionsFrom('METEOR@1.4.3.2');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
   api.addFiles(['synced-cron-server.js'], 'server');
   api.export(['SyncedCron'], 'server');
